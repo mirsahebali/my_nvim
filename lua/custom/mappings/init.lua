de32663ccfcc0fe19ff4b66f4be21a36360c9655
mirsahebali <mirsahebali@protonmail.com>
2024-01-21 15:54:48 +0530

    revamp with new config for rust,flash,etc

--- lua/custom/mappings/init.lua
+++ lua/custom/mappings/init.lua
@@ -1,29 +1,81 @@
-vim.keymap.set('n', ';', ':', { desc = 'Command Mode' })
--- vim.keymap.set({ "n", "v" }, "<leader>ft", "<cmd>Neotree toggle<cr>", { desc = "Neotree Toggle" })
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
 -- Command window
-vim.keymap.set({ 'n', 'v' }, 'cw', 'q:', { desc = 'Command Window' })
--- window Navigation remaps--
-vim.keymap.set({ 'n', 'v', 't' }, '<C-j>', '<C-w>j', { desc = 'Move window down' })
-vim.keymap.set({ 'n', 'v', 't' }, '<C-k>', '<C-w>k', { desc = 'Move window up' })
-vim.keymap.set({ 'n', 'v', 't' }, '<C-l>', '<C-w>l', { desc = 'Move window right' })
-vim.keymap.set({ 'n', 'v', 't' }, '<C-h>', '<C-w>h', { desc = 'Move window left' })
--- Save file
-vim.keymap.set('n', '<leader>sf', '<cmd>w<cr>', { desc = '[S]ave [F]ile' })
--- Tmux Vim Navigation
-vim.keymap.set('n', '<C-h>', '<cmd>TmuxNavigateLeft<cr>', { desc = 'Tmux Navigator Left' })
-vim.keymap.set('n', '<C-l>', '<cmd>TmuxNavigateRight<cr>', { desc = 'Tmux Navigator Right' })
-vim.keymap.set('n', '<C-j>', '<cmd>TmuxNavigateDown<cr>', { desc = 'Tmux Navigator Down' })
-vim.keymap.set('n', '<C-k>', '<cmd>TmuxNavigateUp<cr>', { desc = 'Tmux Navigator Up' })
--- Toggle term with nvim terminal
-vim.keymap.set({ 'n', 't', 'v' }, '<C-\\>', '<cmd>ToggleTerm direction=float<cr>', { desc = 'ToggleTerm' })
-vim.keymap.set({ 'n', 't', 'v' }, '<M-\\>', '<cmd>ToggleTerm direction=vertical size=38<cr>', { desc = 'ToggleTerm' })
+vim.keymap.set({ "n", "v" }, "cw", "q:", { desc = "Command Window" })
 
-vim.keymap.set({ 'n', 't' }, '<leader>lf', '<cmd>lua vim.lsp.buf.format()<cr>', { desc = 'LSP Format' })
+vim.keymap.set({ "n", "v" }, "K", function()
+	require("pretty_hover").hover()
+end, { desc = "Command Window" })
+-- window Navigation remaps--
+vim.keymap.set({ "n", "v", "t" }, "<C-j>", "<C-w>j", { desc = "Move window down" })
+vim.keymap.set({ "n", "v", "t" }, "<C-k>", "<C-w>k", { desc = "Move window up" })
+vim.keymap.set({ "n", "v", "t" }, "<C-l>", "<C-w>l", { desc = "Move window right" })
+vim.keymap.set({ "n", "v", "t" }, "<C-h>", "<C-w>h", { desc = "Move window left" })
+-- Save file
+vim.keymap.set("n", "<leader>fs", "<cmd>w<cr>", { desc = " [F]ile [S]ave" })
+-- Tmux Vim Navigation
+vim.keymap.set("n", "<C-h>", "<cmd>TmuxNavigateLeft<cr>", { desc = "Tmux Navigator Left" })
+vim.keymap.set("n", "<C-l>", "<cmd>TmuxNavigateRight<cr>", { desc = "Tmux Navigator Right" })
+vim.keymap.set("n", "<C-j>", "<cmd>TmuxNavigateDown<cr>", { desc = "Tmux Navigator Down" })
+vim.keymap.set("n", "<C-k>", "<cmd>TmuxNavigateUp<cr>", { desc = "Tmux Navigator Up" })
+-- Toggle term with nvim terminal
+vim.keymap.set({ "n", "t", "v" }, "<C-\\>", "<cmd>ToggleTerm direction=float <cr>", { desc = "ToggleTerm" })
+vim.keymap.set({ "n", "t", "v" }, "<M-\\>", "<cmd>ToggleTerm direction=vertical size=38<cr>", { desc = "ToggleTerm" })
+vim.keymap.set({ "n", "t" }, "<leader>lf", "<cmd>lua vim.lsp.buf.format()<cr>", { desc = "LSP Format" })
 -- require("custom.mappings.mini")
 -- Buffline Mappings
 
-vim.keymap.set('n', '<M-i>', '<cmd>BufferLineCycleNext<cr>', { desc = 'Next Buffer' })
-vim.keymap.set('n', '<M-u>', '<cmd>BufferLineCyclePrev<cr>', { desc = 'Previous Buffer' })
+vim.keymap.set("n", "<M-i>", "<cmd>BufferLineCycleNext<cr>", { desc = "Next Buffer" })
+vim.keymap.set("n", "<M-u>", "<cmd>BufferLineCyclePrev<cr>", { desc = "Previous Buffer" })
 
-vim.keymap.set('n', '<leader>cl', ':bdelete<cr>', { desc = 'Previous Buffer', silent = true })
-vim.keymap.set('n', '<leader>C', '<cmd>BufferLineCloseOthers<cr>', { desc = 'Close all other buffers', silent = true })
+vim.keymap.set("n", "<leader>C", "<cmd>BufferLineCloseOthers<cr>", { desc = "Close all other buffers", silent = true })
+-- Line diagnostics
+vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, { desc = "Line Diagnostics" })
+-- Telescope mappings
+
+vim.keymap.set(
+	"n",
+	"<leader>f/",
+	"<cmd>Telescope current_buffer_fuzzy_find<cr>",
+	{ desc = "Find in current buffer", silent = true }
+)
+
+-- Oil.nvim Keymaps
+vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+
+--MultiCursor Mappings
+vim.keymap.set({ "n", "v" }, "<M-,>", "<cmd>MCstart<cr>", { desc = "Start MultiCursor" })
+
+
+vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
+
+-- Remap for dealing with word wrap
+vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
+vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+
+
+-- See `:help telescope.builtin`
+vim.keymap.set('n', '<leader>?', require('telescope.builtin').oldfiles, { desc = '[?] Find recently opened files' })
+vim.keymap.set('n', '<leader><space>', require('telescope.builtin').buffers, { desc = '[ ] Find existing buffers' })
+vim.keymap.set('n', '<leader>fb', function()
+	-- You can pass additional configuration to telescope to change theme, layout, etc.
+	require('telescope.builtin').current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
+		winblend = 10,
+		previewer = false,
+	})
+end, { desc = ' [F]uzzily search in current [b]uffer' })
+vim.keymap.set('n', '<leader>gf', require('telescope.builtin').git_files, { desc = 'Search [G]it [F]iles' })
+vim.keymap.set('n', '<leader>ff', require('telescope.builtin').find_files, { desc = '[F]ind [F]iles' })
+vim.keymap.set('n', '<leader>fh', require('telescope.builtin').help_tags, { desc = '[F]ind [H]elp' })
+vim.keymap.set('n', '<leader>fw', require('telescope.builtin').grep_string, { desc = '[F]ind current [W]ord' })
+vim.keymap.set('n', '<leader>fg', require('telescope.builtin').live_grep, { desc = '[F]ind by [G]rep' })
+vim.keymap.set('n', '<leader>fd', require('telescope.builtin').diagnostics, { desc = '[F]ind [D]iagnostics' })
+vim.keymap.set('n', '<leader>fF', '<cmd>Telescope find_files hidden=true<cr>', { desc = '[F]ind [F]iles' })
+
+
+-- Diagnostics mappings
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous diagnostic message' })
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
+vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
+vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
--- lua/custom/plugins/colorscheme.lua
+++ lua/custom/plugins/colorscheme.lua
@@ -1,30 +1,126 @@
 return {
 	{
 		"catppuccin/nvim",
+		lazy = true,
 		name = "catppuccin",
-		priority = 1000
-		,
-		config = function()
-			require("catppuccin").setup {
-				transparent_background = true
-			}
-			vim.cmd.colorscheme "catppuccin"
-		end
-	},
+		opts = {
+			flavour = "mocha",
+			color_overrides = {
+				mocha = {
+					base = "#11111B"
 
+				},
+				macchiato = {
+					base = "#222429",
+				},
+			},
+			transparent_background = false,
+			integrations = {
+				alpha = true,
+				cmp = true,
+				flash = true,
+				gitsigns = true,
+				illuminate = true,
+				indent_blankline = { enabled = true },
+				lsp_trouble = true,
+				mason = true,
+				mini = true,
+				native_lsp = {
+					enabled = true,
+					underlines = {
+						errors = { "undercurl" },
+						hints = { "undercurl" },
+						warnings = { "undercurl" },
+						information = { "undercurl" },
+					},
+				},
+				navic = { enabled = true, custom_bg = "lualine" },
+				neotest = true,
+				noice = true,
+				notify = true,
+				neotree = true,
+				semantic_tokens = true,
+				telescope = true,
+				treesitter = true,
+				which_key = true,
+			},
+		},
+	},
 	{
-		"folke/tokyonight.nvim",
-		lazy = false,
-		priority = 1000,
-		opts = {},
+		"navarasu/onedark.nvim",
+		lazy = true,
 		config = function()
-			-- require("tokyonight").setup {
-			-- 	transparent = true,
-			-- 	style = "storm"
-			-- }
-			-- vim.cmd.colorscheme "tokyonight"
-		end
+			require("onedark").setup({
+				style = "deep",
+				transparent = true,
+				diagnostics = {
+					undercurl = false,
+				},
+			})
+		end,
 	},
+	{
+		"Shatur/neovim-ayu",
+		lazy = true,
+		config = function()
+			require("ayu").setup({
 
+				overrides = {
+					Normal = { bg = "None" },
+					ColorColumn = { bg = "None" },
+					SignColumn = { bg = "None" },
+					Folded = { bg = "None" },
+					FoldColumn = { bg = "None" },
+					CursorLine = { bg = "None" },
+					CursorColumn = { bg = "None" },
+					WhichKeyFloat = { bg = "None" },
+					VertSplit = { bg = "None" },
+				},
+			})
+		end,
+	},
+	{
+		"rebelot/kanagawa.nvim",
+		lazy = true,
+		config = function()
+			-- code
+			-- Default options:
+			require("kanagawa").setup({
+				compile = false, -- enable compiling the colorscheme
+				undercurl = true, -- enable undercurls
+				commentStyle = { italic = true },
+				functionStyle = {},
+				keywordStyle = { italic = true },
+				statementStyle = { bold = true },
+				typeStyle = {},
+				transparent = false, -- do not set background color
+				dimInactive = false, -- dim inactive window `:h hl-NormalNC`
+				terminalColors = true, -- define vim.g.terminal_color_{0,17}
+				colors = {
+					theme = {
+						all = {
+							ui = {
+								bg_gutter = "none",
+							},
+						},
+					},
+				},
+				overrides = function(colors) -- add/modify highlights
+					return {}
+				end,
+				theme = "wave", -- Load "wave" theme when 'background' option is not set
+				background = { -- map the value of 'background' option to a theme
+					dark = "wave", -- try "dragon" !
+					light = "lotus",
+				},
+			})
 
+			-- setup must be called before loading
+			vim.cmd("colorscheme kanagawa")
+		end,
+	},
+	{
+		-- If you are using Packer
+		"shaunsingh/solarized.nvim",
+	},
 }
