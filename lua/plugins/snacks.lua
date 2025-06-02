8c2eb1cacbd92820e95649e41054cd6d496d1296
mirsahebali <mirsahebali@pm.me>
2025-06-02 11:25:27 +0530

    disable ghosting configs

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -18,7 +18,7 @@ return {
         -- stylua: ignore
         ---@type snacks.dashboard.Item[]
         keys = {
-          { icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
+          { icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files', { cwd = '.' })" },
           { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
           { icon = " ", key = "g", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
           { icon = " ", key = "s", desc = "Restore Session", section = "session" },
